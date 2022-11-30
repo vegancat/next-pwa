40cfb17596d486ff9ab503c0c711284ac5b83d24
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2022-11-30 15:08:47 +0700

    fix: fixed register.js not found

--- rollup.config.js
+++ rollup.config.js
@@ -3,43 +3,54 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
 
-export default defineConfig({
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/ducanh2912-next-pwa.js",
-      format: "cjs",
-      exports: "named",
-    },
-    {
-      file: "dist/ducanh2912-next-pwa.modern.mjs",
+export default [
+  defineConfig({
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/ducanh2912-next-pwa.js",
+        format: "cjs",
+        exports: "named",
+      },
+      {
+        file: "dist/ducanh2912-next-pwa.modern.mjs",
+        format: "esm",
+      },
+      {
+        file: "dist/ducanh2912-next-pwa.module.js",
+        format: "esm",
+      },
+    ],
+    plugins: [
+      typescript({
+        noForceEmit: true,
+        noEmitOnError: true,
+        outDir: "dist",
+        declaration: true,
+        noEmit: false,
+      }),
+      terser(),
+    ],
+    external: [
+      "clean-webpack-plugin",
+      "terser-webpack-plugin",
+      "workbox-webpack-plugin",
+      "webpack",
+      "crypto",
+      "fs",
+      "path",
+      "url",
+      "globby",
+      "workbox-window",
+    ],
+  }),
+  defineConfig({
+    input: "src/register.js",
+    output: {
+      file: "dist/register.js",
       format: "esm",
     },
-    {
-      file: "dist/ducanh2912-next-pwa.module.js",
-      format: "esm",
-    },
-  ],
-  plugins: [
-    typescript({
-      noForceEmit: true,
-      noEmitOnError: true,
-      outDir: "dist",
-      declaration: true,
-      noEmit: false,
-    }),
-    terser(),
-  ],
-  external: [
-    "clean-webpack-plugin",
-    "terser-webpack-plugin",
-    "workbox-webpack-plugin",
-    "webpack",
-    "crypto",
-    "fs",
-    "path",
-    "url",
-    "globby",
-    "workbox-window",
-  ],
-});
+    plugins: [terser()],
+    external: ["workbox-window"],
+  }),
+];
