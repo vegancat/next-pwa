833f8218fd724d2d3f5abee3a7c546cbf47ed97b
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2023-05-03 10:15:02 +0700

    style(code): modified fallback.ts so that it looks nicer

--- packages/next-pwa/src/fallback.ts
+++ packages/next-pwa/src/fallback.ts
@@ -1,54 +1,29 @@
 self.fallback = async (request) => {
   // https://developer.mozilla.org/en-US/docs/Web/API/RequestDestination
-  switch (request.destination) {
-    case "document":
-      if (process.env.__PWA_FALLBACK_DOCUMENT__) {
-        return caches.match(process.env.__PWA_FALLBACK_DOCUMENT__, {
-          ignoreSearch: true,
-        });
-      }
-      return Response.error();
-    case "image":
-      if (process.env.__PWA_FALLBACK_IMAGE__) {
-        return caches.match(process.env.__PWA_FALLBACK_IMAGE__, {
-          ignoreSearch: true,
-        });
-      }
-      return Response.error();
-    case "audio":
-      if (process.env.__PWA_FALLBACK_AUDIO__) {
-        return caches.match(process.env.__PWA_FALLBACK_AUDIO__, {
-          ignoreSearch: true,
-        });
-      }
-      return Response.error();
-    case "video":
-      if (process.env.__PWA_FALLBACK_VIDEO__) {
-        return caches.match(process.env.__PWA_FALLBACK_VIDEO__, {
-          ignoreSearch: true,
-        });
-      }
-      return Response.error();
-    case "font":
-      if (process.env.__PWA_FALLBACK_FONT__) {
-        return caches.match(process.env.__PWA_FALLBACK_FONT__, {
-          ignoreSearch: true,
-        });
-      }
-      return Response.error();
-    case "":
-      if (
-        process.env.__PWA_FALLBACK_DATA__ &&
-        request.url.match(/\/_next\/data\/.+\/.+\.json$/i)
-      ) {
-        return caches.match(process.env.__PWA_FALLBACK_DATA__, {
-          ignoreSearch: true,
-        });
-      }
-      return Response.error();
-    default:
-      return Response.error();
+  const { destination, url } = request;
+  const fallbackUrl: Partial<Record<RequestDestination, string | undefined>> = {
+    document: process.env.__PWA_FALLBACK_DOCUMENT__,
+    image: process.env.__PWA_FALLBACK_IMAGE__,
+    audio: process.env.__PWA_FALLBACK_AUDIO__,
+    video: process.env.__PWA_FALLBACK_VIDEO__,
+    font: process.env.__PWA_FALLBACK_FONT__,
+  };
+  const fallbackResponse = fallbackUrl[destination];
+  if (fallbackResponse) {
+    return caches.match(fallbackResponse, {
+      ignoreSearch: true,
+    });
   }
+  if (
+    destination === "" &&
+    process.env.__PWA_FALLBACK_DATA__ &&
+    url.match(/\/_next\/data\/.+\/.+\.json$/i)
+  ) {
+    return caches.match(process.env.__PWA_FALLBACK_DATA__, {
+      ignoreSearch: true,
+    });
+  }
+  return Response.error();
 };
 
 // Mark file as module.
